c1e5fdf57a1c752c7b5d0750b1ee0e779a3eb922
Timon van Waardhuizen <timonvanwaardhuizen@gmail.com>
2020-05-12 16:01:49 +0200

    Added 2 scenes for A-B testing

---
+++ Assets/Scripts/ATest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ATest : MonoBehaviour
+{
+    public Image fader;
+    public GameObject camera;
+    private Color alphaChange;
+    private bool startTest = false;
+    private bool startZoom = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        alphaChange = fader.color;
+
+        StartCoroutine(timer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(startTest)
+        {
+            
+                alphaChange.a += 0.5f * Time.deltaTime;
+                fader.color = alphaChange;
+                Debug.Log("in test setter");
+            
+        }
+
+        if (startZoom)
+        {
+            
+          
+                alphaChange.a -= 0.5f * Time.deltaTime;
+                fader.color = alphaChange;
+                Debug.Log("in zoom setter");
+            
+        }
+    }
+
+    private IEnumerator timer()
+    {
+        yield return new WaitForSeconds(2);
+        startTest = true;
+        yield return new WaitForSeconds(2.2f);
+        camera.transform.position = new Vector3(-65,-40,400);
+        startTest = false;
+        startZoom = true;
+    }
+}
---
+++ Assets/Scripts/BScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BScript : MonoBehaviour
+{
+    public Camera cam;
+
+    public GameObject pelvis;
+    private float[] vectors = new float[]{0.5f, 0.5f, 0.5f};
+    private bool stopRotate = false;
+    private bool startTest = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(timer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startTest)
+        {
+            //380
+            if (cam.transform.position.z >= 385)
+            {
+                if (!stopRotate)
+                {
+                    //pelvis
+                    vectors[0] += Random.Range(0.1f, 0.6f);
+                    vectors[1] += Random.Range(0.1f, 0.6f);
+                    vectors[2] += Random.Range(0.1f, 0.6f);
+
+                    pelvis.transform.Rotate(vectors[0], vectors[1], vectors[2], Space.Self);
+                }
+            }
+            else
+            {
+                cam.transform.Translate(Vector3.forward * (Time.deltaTime * 500));
+            }
+        }
+    }
+
+    private IEnumerator timer()
+    {
+        yield return new WaitForSeconds(1);
+        startTest = true;
+        yield return new WaitForSeconds(2);
+        stopRotate = true;
+    }
+}
