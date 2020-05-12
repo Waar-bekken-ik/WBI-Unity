afc25789fc31cfb5e66c60451e20b759be34b3ea
Timon van Waardhuizen <timonvanwaardhuizen@gmail.com>
2020-05-12 02:00:29 +0200

    boiler plate

--- Assets/Scripts/API.cs
+++ Assets/Scripts/API.cs
@@ -7,7 +7,30 @@ using UnityEngine.UI;
 
 public class API : MonoBehaviour
 {
+    #region Singleton
+    private static API _instance;
+    public static API Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                // API api = new API("API");
+                // api.AddComponent<API>();
+            }
+
+            return _instance;
+        }
+    }
+    #endregion
+
     private readonly string baseURL = "https://pokeapi.co/api/v2/";
+
+    void Awake()
+    {
+        //start values
+        _instance = this;
+    }
 
     private void Start()
     {
@@ -15,7 +38,7 @@ public class API : MonoBehaviour
     }
 
     //change name to more appropiate name for the job ;)
-    IEnumerator GetData()
+    private IEnumerator GetData()
     {
         Debug.Log("API ROUTINE STARTED");
 
@@ -47,6 +70,27 @@ public class API : MonoBehaviour
         Debug.Log("MADE IT SHIT FAR");
 
         Debug.Log(pokeName + "\n" + pokeSPriteURL + "\n" + pokeTypeNames.ToString());
+    }
+
+    //change name to more appropiate name for the job ;)
+    private IEnumerator SendData()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("myField", "myData");
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http://www.my-server.com/myform", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+            }
+        }
     }
 
     private string CapitializeFirstCharacter(string str)
---
+++ Assets/Scripts/MakeGameUI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MakeGameUI : MonoBehaviour
+{
+    public InputField rounds;
+    public InputField time;
+    public Dropdown question;
+
+    public Text roomCode;
+    public Text people;
+
+    public GameObject makePanel;
+    public GameObject startPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MakeGame()
+    {
+        makePanel.SetActive(false);
+        startPanel.SetActive(true);
+        //send data
+    }
+
+    public void StartGame()
+    {
+
+    }
+}
